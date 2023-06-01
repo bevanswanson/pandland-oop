f1008babd0bda021303d8569189ce900d4666eba
Bevan Swanson <bev@rivling.com>
2023-06-01 16:23:36 +0100

    added README.md and scaffolded problems

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# oaklandas
+# Pandland
+
+Welcome to the High Street Discount Data Consultancy _Pandland_!  We are growing rapidly, and need to provide some BI metrics to measure how we've been doing.  Unfortunately, our homemade Data Science library, `oaklandas` is broken!  Can _you_ fix it before the board meeting tomorrow?!
--- oaklandas/oaklandas.py
+++ oaklandas/oaklandas.py
@@ -1,10 +1,13 @@
+import csv
+
+
 class DataFrame:
     def __init__(self, data=None, columns=None):
         self.data = data
         self.columns = columns
 
-    def __repr__(self):
-        return str(self.data)
+    # def __repr__(self):
+    #     return str(self.data)
 
     def set_data(self, data):
         self.data = data
@@ -20,9 +23,20 @@ class DataFrame:
         if self.data is not None:
             return self.data[-n:]
 
-    def shape(self):
-        if self.data is not None:
-            return len(self.data), len(self.columns)
+    # def shape(self):
+    #     if self.data is not None:
+    #         return len(self.data), len(self.columns)
+
+    def append(self, data, header=None):
+        if header:
+            data = [ row for row in csv_reader ]
+            columns = data.pop(0)
+            self.data.append(data)
+        else:
+            self.data.append(data)
+
+    def get_column(self, column_name):
+        pass
 
     def info(self):
         if self.data is not None:
@@ -30,3 +44,10 @@ class DataFrame:
             for col in self.columns:
                 print(col)
             print("dtypes: object")
+
+def df_from_csv(path):
+    with open(path, 'r') as file:
+        csv_reader = csv.reader(file)
+        data = [ row for row in csv_reader ]
+        columns = data.pop(0)
+    return DataFrame(data=data, columns=columns)
